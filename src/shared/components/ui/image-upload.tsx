82fd4e93598b3e63e78a2881b497ab647499e0b4
shahab <shahab5191@gmail.com>
2023-03-28 23:37:09 +0330

    migrating image Upload Component to type script

--- src/shared/components/ui/image-upload.tsx
+++ src/shared/components/ui/image-upload.tsx
@@ -1,30 +1,40 @@
 import { useEffect, useRef, useState } from "react";
 import SvgIcon from "./icons";
+import React from "react";
 
-const ImageUpload = (props) => {
+interface Props {
+    onImageChange: Function,
+}
+
+const ImageUpload = ({ onImageChange }: Props) => {
     const [fileName, setFileName] = useState("");
-    const [imageFile, setImageFile] = useState();
-    const openRef = useRef();
+    const [imageFile, setImageFile] = useState<File>();
+
+    const openRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         if (imageFile) {
-            props.onImageChange(imageFile);
+            onImageChange(imageFile);
         }
     }, [imageFile])
 
 
-    const openDialogueHandler = (e) => {
-        let file = e.target.files[0];
-        setImageFile(file);
-        let imageUrl = URL.createObjectURL(file);
-        setFileName(imageUrl)
+    const openDialogueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.currentTarget.files) {
+            let file = e.currentTarget.files[0];
+            console.log(file);
+            
+            setImageFile(file);
+            let imageUrl = URL.createObjectURL(file);
+            setFileName(imageUrl)
+        }
     }
 
     const clearFileHandler = () => {
         setFileName("");
     }
 
-    const dropHandler = (e) => {
+    const dropHandler = (e: React.DragEvent<HTMLButtonElement>) => {
         e.preventDefault();
         e.currentTarget.classList.remove("border-[3px]");
         e.currentTarget.classList.add("border-[2px]")
@@ -38,12 +48,12 @@ const ImageUpload = (props) => {
         }
         if (imageUrl !== "") setFileName(imageUrl);
     }
-    const dragOverHandler = (e) => {
+    const dragOverHandler = (e:React.DragEvent<HTMLButtonElement>) => {
         e.preventDefault();
         e.currentTarget.classList.remove("border-[2px]")
         e.currentTarget.classList.add("border-[3px]")
     }
-    const dragExitHandler = (e) => {
+    const dragExitHandler = (e:React.DragEvent<HTMLButtonElement>) => {
         e.preventDefault();
         e.currentTarget.classList.remove("border-[3px]");
         e.currentTarget.classList.add("border-[2px]")
@@ -63,13 +73,13 @@ const ImageUpload = (props) => {
                 onDrop={dropHandler}
                 onDragOver={dragOverHandler}
                 onDragLeave={dragExitHandler}
-                onClick={() => { openRef.current.click(); }}
+                onClick={() => { openRef.current?.click(); }}
                 className="w-full h-[150px] border-border dark:border-border-dark border-[2px] rounded-lg dark:bg-primary-dark bg-primary grid items-center justify-center border-dashed transition-[border]">
                 <SvgIcon icon="uploadImageIcon" className="fill-none stroke-[.3px] stroke-border-dark" width="100" height="100" />
             </button>
             {fileName ?
                 <>
-                    <img src={fileName} className="absolute top-0 left-0 object-cover w-full h-full" />
+                    <img src={fileName} className="absolute top-0 left-0 object-cover w-full h-full" alt="Upload Food Preview"/>
                     <div
                         onClick={clearFileHandler}
                         className="absolute top-0 right-0 z-20 w-[40px] bg-primary dark:bg-primary-dark text-center select-none cursor-pointer hover:bg-button dark:hover:bg-button-dark">X</div>
