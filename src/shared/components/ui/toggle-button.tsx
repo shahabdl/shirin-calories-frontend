05db9ebc9abb88914738e75abd0fc258453240cd
shahab <shahab5191@gmail.com>
2023-03-29 15:47:48 +0330

    migrating toggleButton to typescript

--- src/shared/components/ui/toggle-button.tsx
+++ src/shared/components/ui/toggle-button.tsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
+import React from "react";
 import SvgIcon from "./icons";
 
-const ToggleButton = ({ checkedIcon, unCheckedIcon, onChange, defaultState, className }) => {
+interface Props {
+    checkedIcon: string,
+    unCheckedIcon: string,
+    onChange: Function,
+    defaultState: boolean,
+    className:string
+}
+
+const ToggleButton = ({ checkedIcon, unCheckedIcon, onChange, defaultState, className }:Props) => {
     const [checked, setChecked] = useState(true);
 
+    const toggleStateHandler = () =>{
+        onChange(!checked);
+        setChecked(!checked);
+    }
+    
     useEffect(() => {
         setChecked(defaultState);
     }, [])
 
-    useEffect(() => {
-        onChange(checked);
-    }, [checked])
-
     return (
         <div
-            onClick={() => { setChecked(!checked) }}
+            onClick={toggleStateHandler}
             className={`dark:bg-secondary-dark bg-button-dark w-[60px] h-[30px] border-border dark:border-border-dark border-[1px] rounded-full relative cursor-pointer transition-colors duration-300 ${className}`}>
             <div
                 className={`${checked ? "left-[2px]" : "left-[calc(100%-26px)]"} absolute top-[50%] translate-y-[-50%] w-[24px] h-[24px] rounded-full dark:bg-primary-dark bg-primary border-border dark:border-border-dark border-[1px] transition-[left] duration-300 grid items-center justify-center`}
