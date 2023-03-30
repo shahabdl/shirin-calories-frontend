e3d2a6faf92f2321d1216e03c7b75959b24c057d
shahab <shahab5191@gmail.com>
2023-03-30 05:01:00 +0330

    new test for InputNumber component

--- src/shared/components/ui/tests/input-number.test.tsx
+++ src/shared/components/ui/tests/input-number.test.tsx
@@ -1,48 +1,74 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import InputNumber from '../input-number';
 import userEvent from '@testing-library/user-event'
 
-test('default value must be 0 if it is not specified in Component initializing',()=>{
-    render(<InputNumber onChange={jest.fn()}/>);
-    const inputBox = screen.getByRole('spinbutton', {name: "number-input-box"})  as HTMLInputElement;
+test('default value must be 0 if it is not specified in Component initializing', () => {
+    const mockOnChangeFunction = jest.fn();
+    render(<InputNumber onChange={mockOnChangeFunction} />);
+    const inputBox = screen.getByRole('spinbutton', { name: "number-input-box" }) as HTMLInputElement;
+    //input box value should be 0 if there is no defaultValue
     expect(parseFloat(inputBox.value)).toBe(0);
+    //first parameter of first call of onChange callback should be 0 when there is no defaultValue
+    expect(mockOnChangeFunction.mock.calls[0][0]).toBe(0);
+
 })
 
-test('default value must be same as initialized value',()=>{
-    render(<InputNumber defaultValue={5} onChange={jest.fn()}/>);
-    const inputBox = screen.getByRole('spinbutton', {name:"number-input-box"}) as HTMLInputElement;
+test('default value must be same as initialized value', () => {
+    const mockOnChangeFunction = jest.fn();
+    render(<InputNumber defaultValue={5} onChange={mockOnChangeFunction} />);
+    const inputBox = screen.getByRole('spinbutton', { name: "number-input-box" }) as HTMLInputElement;
+    //input text should be Default value
     expect(parseFloat(inputBox.value)).toBe(5);
+    //onChange callback function first parameter of first call should be 5
+    expect(mockOnChangeFunction.mock.calls[0][0]).toBe(5);
 })
 
-test('value must increase by clicking on + button', ()=>{
-    render(<InputNumber onChange={jest.fn()} />);
-    const inputBox = screen.getByRole('spinbutton', {name:"number-input-box"}) as HTMLInputElement;;
+test('value must increase by clicking on + button', () => {
+    const mockOnChangeFunction = jest.fn();
+    render(<InputNumber onChange={mockOnChangeFunction} />);
+    const inputBox = screen.getByRole('spinbutton', { name: "number-input-box" }) as HTMLInputElement;;
     const incrementButton = screen.getByText('+');
     userEvent.click(incrementButton);
+    //input text should change to new value (last value + 1) when user clicks on + button
     expect(parseFloat(inputBox.value)).toBe(1);
+    //first parameter of second call of onChange callback should be new value when user clicks on + button, first call is initial value
+    expect(mockOnChangeFunction.mock.calls[1][0]).toBe(1);
 })
 
-test('value must decrease by clicking on - button', ()=>{
-    render(<InputNumber defaultValue={1} onChange={jest.fn()}/>);
-    const inputBox = screen.getByRole('spinbutton', {name:"number-input-box"}) as HTMLInputElement;;
-    const incrementButton = screen.getByText('-');
-    userEvent.click(incrementButton);
+test('value must decrease by clicking on - button', () => {
+    const mockOnChangeFunction = jest.fn();
+    render(<InputNumber defaultValue={1} onChange={mockOnChangeFunction} />);
+    const inputBox = screen.getByRole('spinbutton', { name: "number-input-box" }) as HTMLInputElement;;
+    const decrementButton = screen.getByText('-');
+    userEvent.click(decrementButton);
+    //input text should change to new value (last value - 1) when user clicks on - button
     expect(parseFloat(inputBox.value)).toBe(0);
+    //first parameter of second call of onChange callback should be new value when user clicks on - button, first call is initial value
+    expect(mockOnChangeFunction.mock.calls[1][0]).toBe(0);
 })
 
-test('when negative is false input should stay greater than 0',()=>{
-    render(<InputNumber defaultValue={0.5} onChange={jest.fn()}/>);
-    const inputBox = screen.getByRole('spinbutton', {name:"number-input-box"}) as HTMLInputElement;;
-    const incrementButton = screen.getByText('-');
-    userEvent.click(incrementButton);
+test('when negative is false input should stay greater than 0', () => {
+    const mockOnChangeFunction = jest.fn();
+    render(<InputNumber defaultValue={0.5} onChange={mockOnChangeFunction} />);
+    const inputBox = screen.getByRole('spinbutton', { name: "number-input-box" }) as HTMLInputElement;;
+    const decrementButton = screen.getByText('-');
+    userEvent.click(decrementButton);
+    //input text should stay greater than 0 when user clicks on - button
     expect(parseFloat(inputBox.value)).toBe(0.5);
+    //onChange callback should not call when input value stays the same
+    expect(mockOnChangeFunction.mock.calls).toHaveLength(1);
+
 })
 
-test('when negative is true input can be less than 0',()=>{
-    render(<InputNumber defaultValue={0.5} canBeNegative={true} onChange={jest.fn()}/>);
-    const inputBox = screen.getByRole('spinbutton', {name:"number-input-box"}) as HTMLInputElement;;
+test('when negative is true input can be less than 0', () => {
+    const mockOnChangeFunction = jest.fn();
+    render(<InputNumber defaultValue={0.5} canBeNegative={true} onChange={mockOnChangeFunction} />);
+    const inputBox = screen.getByRole('spinbutton', { name: "number-input-box" }) as HTMLInputElement;;
     const incrementButton = screen.getByText('-');
     userEvent.click(incrementButton);
+    //input text should change to new value (last value - 1) when user clicks on - button
     expect(parseFloat(inputBox.value)).toBe(-0.5);
+    //first parameter of second call of onChange callback should be new value when user clicks on - button, first call is initial value
+    expect(mockOnChangeFunction.mock.calls[1][0]).toBe(-0.5);
 })
