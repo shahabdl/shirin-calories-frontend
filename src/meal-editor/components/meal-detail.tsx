561dd6efb9d3a617f35d15df37fbca7061922051
shahab <shahab5191@gmail.com>
2023-03-31 15:07:29 +0330

    Migrating MealDetail to typescript

--- src/meal-editor/components/meal-detail.tsx
+++ src/meal-editor/components/meal-detail.tsx
@@ -2,29 +2,34 @@ import ImageUpload from "../../shared/components/ui/image-upload"
 import DropdownList from "../../shared/components/ui/dropdown-list"
 import InputNumber from "../../shared/components/ui/input-number";
 import { useContext } from "react";
+import React from "react";
 import { ingredientContext } from "../../context/meal-context";
 
-const MealDetailes = ({onImageChange}) => {
+interface PropsType {
+    onImageChange: (image: string) => null
+}
+
+const MealDetailes = ({ onImageChange }: PropsType) => {
     const { state, dispatch } = useContext(ingredientContext)
-    const changeWeight = (weight) => {
+    const changeWeight = (weight: number) => {
         dispatch({ type: "changeFoodProperties", payload: { foodWeight: weight } })
     }
-    const changeUnit = (unit) => {
+    const changeUnit = (unit: string) => {
         dispatch({ type: "changeFoodProperties", payload: { foodUnit: unit } })
     }
 
-    const changeName = (name) => {
+    const changeName = (name: string) => {
         dispatch({ type: "changeFoodProperties", payload: { foodName: name } })
     }
 
-    const submitHandler = (e) => {
+    const submitHandler = (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
     }
 
     return (
         <form className="px-3 mt-2 pb-5 mb-2 border-b-[1px] border-border dark:border-border-dark" onSubmit={submitHandler}>
             <h2 className="text-lg font-light pb-2">Food Properties</h2>
-            <ImageUpload onImageChange={(image)=>{onImageChange(image)}} stateImage={state.foodProperties.foodImage}/>
+            <ImageUpload onImageChange={(image:string) => { onImageChange(image) }}  />
             <input
                 onChange={(e) => changeName(e.target.value)}
                 className="w-full rounded-lg h-[34px] dark:bg-primary-dark bg-primary dark:border-border-dark border-border border-[1px] focus-visible:outline-none dark:focus-visible:bg-background-dark focus-visible:bg-background px-2 font-light mb-2 transition-colors"
@@ -34,10 +39,10 @@ const MealDetailes = ({onImageChange}) => {
             <div className="grid grid-cols-2 gap-2">
                 <InputNumber
                     onChange={changeWeight}
-                    default={state.foodProperties.foodWeight? state.foodProperties.foodWeight : 100}
+                    defaultValue={state.foodProperties.foodWeight ? state.foodProperties.foodWeight : 100}
                 />
                 <DropdownList
-                    default={state.foodProperties.foodUnit? state.foodProperties.foodUnit : "g"}
+                    defaultValue={state.foodProperties.foodUnit ? state.foodProperties.foodUnit : "g"}
                     onSelect={changeUnit}
                     options={["g", "oz", "lb"]}
                 />
